1f66e47870fa3f1b2116b9d07f496f04ec83dde7
root <root@TasnimSomo>
2024-05-31 08:46:07 +0400

    new changes

--- update_passwords.py
+++ update_passwords.py
@@ -10,8 +10,7 @@ bcrypt = Bcrypt()
 def update_passwords():
     users = session.query(Customer).all()
     for user in users:
-        # Assuming you have a way to retrieve or reset the user's password
-        new_password = 'new_password_for_user'  # Replace with actual password retrieval logic
+        new_password = 'new_password_for_user'
         hashed_password = bcrypt.generate_password_hash(new_password).decode('utf-8')
         user.password = hashed_password
     session.commit()
